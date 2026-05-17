2ff895c7933012c9df2a409a39e7400817c04cc3
setaarya93-glitch <setaarya93@gmail.com>
2026-05-17 21:05:45 +0700

    fix: dynamically create required storage directories in /tmp

--- api/index.php
+++ api/index.php
@@ -7,4 +7,29 @@
  * This forwards the request to the standard Laravel public/index.php
  */
 
+/*
+|--------------------------------------------------------------------------
+| Create Writable Directories in /tmp
+|--------------------------------------------------------------------------
+|
+| Since Vercel has a read-only filesystem, we redirect storage to /tmp/storage.
+| However, Laravel expects certain subdirectories to exist. We create them here
+| if they don't already exist.
+|
+*/
+if (isset($_ENV['VERCEL']) || getenv('VERCEL')) {
+    $requiredDirectories = [
+        '/tmp/storage/framework/cache/data',
+        '/tmp/storage/framework/sessions',
+        '/tmp/storage/framework/views',
+        '/tmp/storage/app/public',
+    ];
+    
+    foreach ($requiredDirectories as $directory) {
+        if (!is_dir($directory)) {
+            mkdir($directory, 0755, true);
+        }
+    }
+}
+
 require __DIR__ . '/../public/index.php';
